c681534e36218c351c2a26cd6c900dfdf235acf1
Daanvdhof <daanvdhof@gmail.com>
2016-05-10 11:56:30 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "top.h"
+
+
+int main()
+{
+    InitBottom();
+    DrawCircle(200,200,10,0);
+    DrawScreen();
+    return 0;
+}
---
+++ middle.h
@@ -0,0 +1,3 @@
+#include "bottom.h"
+void DrawCircle(int x, int y, int radius, int isFilled);
+void DrawRectangle(int x, int y, int width, int height);
