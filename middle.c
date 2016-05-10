9998c108b8baba4f8a0175d15b470ae42413f8f0
Daanvdhof <daanvdhof@gmail.com>
2016-05-10 13:42:18 +0200

    Update middle.c

--- middle.c
+++ middle.c
@@ -7,13 +7,11 @@ void DrawCircle(int x, int y, int radius, int isFilled)
     {
         for(j = 0; j < radius*2; j++)
         {
-            if(sqrt(pow(radius-j,2) + pow(radius-i,2) < radius))
-                DrawPixel(x+ i,y+ j, '1');
+            if(ceil(sqrt(pow(radius-j,2) + pow(radius-i,2))) == radius-1)
+                DrawPixel(x+ i,y+ j, 'w');
         }
 
     }
-    if(isFilled == 0)
-        DrawCircle(x,y,radius-1,1);
 
 
 }
